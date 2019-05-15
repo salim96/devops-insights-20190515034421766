f2ca785f5a9af2da8d5a586497618b91407b975b
Salim Al Farsi <salimalfarsi96@gmail.com>
2019-05-15 10:10:16 +0000

    update test

--- tests/server/apiv1.spec.js
+++ tests/server/apiv1.spec.js
@@ -29,7 +29,7 @@
 
   describe('Get Weather', function() {
 
-    it('with without zip code', function() {
+    it('with without city name', function() {
       reqMock = {
         query: {
 
@@ -41,10 +41,10 @@
       assert(resMock.status.lastCall.calledWith(400), 'Unexpected status code:' + resMock.status.lastCall.args);
     });
 
-    it('with valid zip code and error from request call', function() {
+    it('with valid city name and error from request call', function() {
       reqMock = {
         query: {
-          zip: 79968
+          name: 'Hamilton'
         }
       };
 
@@ -60,10 +60,10 @@
       assert(resMock.send.lastCall.calledWith('Failed to get the data'), 'Unexpected response:' + resMock.send.lastCall.args);
     });
 
-    it('with incomplete zip code', function() {
+    it('with incomplete city name', function() {
       reqMock = {
         query: {
-          zip: 79968
+          city: 'Hamilton'
         }
       };
 
@@ -79,23 +79,23 @@
       assert(resMock.send.lastCall.args[0].msg === 'Failed', 'Unexpected response:' + resMock.send.lastCall.args);
     });
 
-    it('with valid zip code', function() {
+    it('with valid city name', function() {
       reqMock = {
         query: {
-          zip: 79968
+          city: 'Hamilton'
         }
       };
 
       var body = {
         cod: 200,
-        name: 'El Paso',
+        name: 'Hamilton',
         weather: [
           {
             main: 'cold'
           }
         ],
         main: {
-          temp: 78
+          temp: 13.05
         }
       };
 
