888f9703a77fffe95fd319ff1a0013d2e8d9e2e3
Salim Al Farsi <salimalfarsi96@gmail.com>
2019-05-16 11:51:35 +0000

    add map

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -67,3 +67,13 @@ ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$ti
     };
     
 }]);
+
+function initMap() {
+  // The location of Uluru
+  var uluru = {lat: -25.344, lng: 131.036};
+  // The map, centered at Uluru
+  var map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 4, center: uluru});
+  // The marker, positioned at Uluru
+  var marker = new google.maps.Marker({position: uluru, map: map});
+}
