bd000a8e12c9eabefe41fc6fc3a3d1ea1e2d432a
Salim Al Farsi <salimalfarsi96@gmail.com>
2019-05-16 01:10:43 +0000

    accept city name

--- static/js/controllers.js
+++ static/js/controllers.js
@@ -30,10 +30,10 @@ ConsoleModule.controller('wcontroller', ['$scope', '$http', '$routeParams', '$ti
             data = $scope.zip4m;
         } 
 
-        if(data.length === 5) {
+        if(data.length !== 0) {
             $http({
                 method: "GET",
-                url: '/api/v1/getWeather?zip=' + data
+                url: '/api/v1/getWeather?q=' + data
             }).then( function(response) {
                 if(which === 1) {
                     $scope.zip1City = response.data.city;
