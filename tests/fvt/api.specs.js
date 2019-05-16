08cd4f0145e9773797a21623db3c9f5ac51d1d31
Salim Al Farsi <salimalfarsi96@gmail.com>
2019-05-16 09:05:16 +0000

    update fvt

--- tests/fvt/api.specs.js
+++ tests/fvt/api.specs.js
@@ -67,7 +67,7 @@
           	} else {
               assert.equal(resp.statusCode, 200);
               var pbody = JSON.parse(body);
-              assert(pbody.name === 'Auckland', "City name does not match");
+              assert(pbody.city === 'Auckland', "City name does not match");
               done();
             }
         });
